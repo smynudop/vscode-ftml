abfe395800348054168eed75acc66a50048b8561
udop <smyn326@gmail.com>
2026-05-10 20:01:48 +0900

    サジェスト機能をついあk

--- src/extension.ts
+++ src/extension.ts
@@ -20,12 +20,62 @@ import {
 } from "./components/listeners";
 import { serveBackend } from "./components/source";
 
+const wikidotTags = [
+	"module",
+	"include",
+	"collapsible",
+	"code",
+	"tabs",
+	"tab",
+];
+
+function provideWikidotTagCompletions(
+	document: vscode.TextDocument,
+	position: vscode.Position,
+) {
+	const line = document.lineAt(position.line).text;
+	const textBeforeCursor = line.slice(0, position.character);
+	const match = textBeforeCursor.match(/\[\[(\/?)([-a-z0-9]*)$/i);
+	if (!match) {
+		return;
+	}
+
+	const typedTag = match[2].toLowerCase();
+	const replacementStart = position.translate(0, -match[2].length);
+
+	return wikidotTags
+		.filter((tag) => tag.toLowerCase().startsWith(typedTag))
+		.map((tag) => {
+			const item = new vscode.CompletionItem(
+				tag,
+				vscode.CompletionItemKind.Keyword,
+			);
+
+			item.detail = match[1] ? "Wikidot closing tag" : "Wikidot tag";
+			item.range = new vscode.Range(replacementStart, position);
+			item.insertText = tag;
+
+			return item;
+		});
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	setContext(context);
 	initInfo();
 	setLockedPreviews(context.workspaceState.get("wikidot.lockedPreviews"));
 
 	context.subscriptions.push(
+		vscode.languages.registerCompletionItemProvider(
+			{ language: "wikidot" },
+			{
+				provideCompletionItems(document, position) {
+					return provideWikidotTagCompletions(document, position);
+				},
+			},
+			"[",
+			"/",
+		),
+
 		vscode.window.registerWebviewPanelSerializer("wikidot.preview", {
 			async deserializeWebviewPanel(
 				webviewEditor: vscode.WebviewPanel,
