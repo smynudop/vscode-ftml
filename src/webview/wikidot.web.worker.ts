00978e3779c24406c224802fb8161dc7b2b81a46
udop <smyn326@gmail.com>
2026-06-12 17:29:49 +0900

    パッケージを更新

--- src/webview/wikidot.web.worker.ts
+++ src/webview/wikidot.web.worker.ts
@@ -69,6 +69,8 @@ async function fetchAllIncludes(source: string): Promise<Record<string, string |
 self.onmessage = async (e: MessageEvent<WorkerRequest>) => {
 	const { id, source, url } = e.data;
 
+	if(!source) return;
+
 	const page: Parser.PageData = {
 		name: "name",
 		category: "category",
